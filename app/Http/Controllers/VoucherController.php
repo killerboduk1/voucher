240c1bb8af68409cf58c2faf5f3a8c67098e574a
gregbalonzo <bodukbalonzo@gmail.com>
2024-07-13 12:05:26 +0800

    add: voucher migration, model, controller, factory, seeder files

---
+++ app/Http/Controllers/VoucherController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Voucher;
+use http\Env\Request;
+
+class VoucherController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+       //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store()
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Voucher $voucher)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Voucher $voucher)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Voucher $voucher)
+    {
+        //
+    }
+}
