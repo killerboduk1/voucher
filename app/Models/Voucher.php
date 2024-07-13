701c7446f7e1d6f25994199058fe3a959500c27d
gregbalonzo <bodukbalonzo@gmail.com>
2024-07-13 21:48:56 +0800

    change: add relationship to tables User and Voucher

--- app/Models/Voucher.php
+++ app/Models/Voucher.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Voucher extends Model
 {
@@ -12,4 +13,14 @@ class Voucher extends Model
     protected $fillable = [
         'voucher'
     ];
+
+    /**
+     * Get the user that owns the voucher
+     *
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
