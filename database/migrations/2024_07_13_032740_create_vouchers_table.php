6c28839e9687258388ed1847e8c8865cf40ee826
gregbalonzo <bodukbalonzo@gmail.com>
2024-07-13 21:46:35 +0800

    change: update migration for voucher add unique

--- database/migrations/2024_07_13_032740_create_vouchers_table.php
+++ database/migrations/2024_07_13_032740_create_vouchers_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('vouchers', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->string('voucher');
+            $table->string('voucher')->unique();
             $table->timestamps();
         });
     }
