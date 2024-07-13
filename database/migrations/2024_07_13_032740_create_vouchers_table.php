276495aedaf3be35e010c4a7375434b3fb667f6a
gregbalonzo <bodukbalonzo@gmail.com>
2024-07-13 18:54:27 +0800

    change: update Vouchers table add relation to Users table

--- database/migrations/2024_07_13_032740_create_vouchers_table.php
+++ database/migrations/2024_07_13_032740_create_vouchers_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('vouchers', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('voucher');
             $table->timestamps();
         });
