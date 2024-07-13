32262e08f6960c05488ca1b765a8c4a124e0b154
gregbalonzo <bodukbalonzo@gmail.com>
2024-07-13 18:37:59 +0800

    change: update api.php add route for register, login, logout

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,12 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\VoucherController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('vouchers', VoucherController::class);
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])
+    ->middleware('auth:sanctum');
