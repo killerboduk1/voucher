d9daa54a89afa84c302ba9d1ebefcbcc79140665
gregbalonzo <bodukbalonzo@gmail.com>
2024-07-14 12:25:18 +0800

    change: update UserFactory.php and VoucherFactory.php for test case requirement

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,10 +24,11 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'username' => $this->faker->userName,
+            'first_name' => $this->faker->firstName,
+            'email' => $this->faker->unique()->safeEmail,
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => bcrypt('password'), // password
             'remember_token' => Str::random(10),
         ];
     }
